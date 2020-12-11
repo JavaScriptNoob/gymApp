bed57c6a7c0932819afbaa8b660f718248b3de7f
JavascripNoob <timur.bobylev@yandex.ru>
2020-12-11 13:34:34 +0100

    refactoring

--- src/wakeupapp/SsnValidation.java
+++ src/wakeupapp/SsnValidation.java
@@ -4,38 +4,38 @@ public class SsnValidation {
     private final String ssn;
     public boolean valid;
 
-    public  SsnValidation(String ssnnumber) {
-        this.ssn = ssnnumber;
-        this.valid = this.ssn.length() == 10 && this.validateCardNumber();
+    public  SsnValidation(String ssnnumber) { // deklarerar konstruktor
+        this.ssn = ssnnumber;//ssn är global variable lika med argument
+        this.valid = this.ssn.length() == 10 && this.validateCardNumber();//variabeln valid har boolean value som beror på resultat av validation
     }
 
     private boolean validateCardNumber() {
         String value = this.ssn;
         int n = value.length() - 1; // 9
         int sum = 0;
-        int controlNumber = Integer.parseInt(value.substring(n));
+        int controlNumber = Integer.parseInt(value.substring(n));// vi sparar kontrollnummer i en separat variabeln
         System.out.println(controlNumber + "control");
-        String valueWithoutControlNumber = value.substring(0, n);
-        System.out.println(n + " Количество итераций");
+        String valueWithoutControlNumber = value.substring(0, n);// Vi klipper ut allt förutom controlnummer
+
 
         for (int index = 0; index < n; index++) {
 
-            int digit = valueWithoutControlNumber.charAt(index) - '0';
+            int digit = valueWithoutControlNumber.charAt(index) - '0';//med hjälp av loop och method charAt vi kan jobba med varje karakter  i string separat
             System.out.println(digit);
-            if (index % 2 == 0) {
+            if (index % 2 == 0) {// om division till 2 lämnar noll i resten. Numret är jämnt eller noll.Det står i uppgiften att vi måste gå från vänstern till höger
 
 
-                digit = digit * 2;
+                digit = digit * 2;//multiplicerar med 2
                 System.out.println("by2");
-                if (digit > 9) {
+                if (digit > 9) {//om value tvåsiffrig då vi subtraherar det med 9. Jag tror att det är stor hack 12 === 1 +2 ===12 - 9
                     digit -= 9;
                 }
             }
 
-            sum += digit;
+            sum += digit;//adderar integer till sum
             System.out.println(sum + "sum");
         }
-        return ((sum + controlNumber) % 10 == 0);
+        return ((sum + controlNumber) % 10 == 0);// räknar ut resultat tillsamans med kontrolnummer
     }
 }
 
--- src/wakeupapp/wakeUpMain.java
+++ src/wakeupapp/wakeUpMain.java
@@ -4,52 +4,52 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class wakeUpMain {
-    public static String personnummer;
+public class wakeUpMain { //vi skapar class
+    public static String personnummer; //vi deklarerar variabeln som tillg'nglig i hella
     public static void main(String[] args) {
         //SsnValidation ssnvalidation = new SsnValidation();
         //ssnvalidation.doTheThings();
-        doTheThings();
+        doTheThings(); // vi  kallar  initial method
     }
 
 
-    private static void doTheThings() {
+    private static void doTheThings() { // initial method
 
 
         Scanner scanner = new Scanner(System.in);
-        int navigateInMenu = 0;
+        int navigateInMenu = 0;//deklarerar variabeln som ska hålla kundens val i framtiden
 
-        do {
-            System.out.println(
+        do { // börjar loop
+            System.out.println( //valalternativ
                     "1. Bli medlem\n" +
                             "2. Logga in\n" +
                             "3. Boka plats på aktivitet\n" +
-                            "4. Avsluta++++++++");
-            System.out.println("Type in your choise");
+                            "4. Avsluta");
+            System.out.println("Skriva in din val");
 
-            navigateInMenu = Integer.parseInt(scanner.next());
-            System.out.println(navigateInMenu);
+            navigateInMenu = Integer.parseInt(scanner.next());//omvandlar datatype from scanner till integer
+
 
         } while (navigateInMenu < 1 || navigateInMenu > 4);
 
         System.out.println("Type in SSN");
-        personnummer = scanner.next();
+        personnummer = scanner.next(); //vi scannar personnumer
         System.out.println(personnummer);
 
-        SsnValidation validation = new SsnValidation(personnummer);
+        SsnValidation validation = new SsnValidation(personnummer); // skapar en objekt av  klass SsnValidation
 
-        if (!validation.valid) {
-            System.out.println("not enjuph");
+        if (!validation.valid) { // om vi returnerar då ssn är felaktigt
+            System.out.println("inte tillräckligt");
             return;
         }
 
-        boolean exit = false;
+        boolean exit = false; // vi behöver det för att bryta do medan loop
 
         do {
-            switch (navigateInMenu) {
+            switch (navigateInMenu) {// switch tar kundens val som argument
                 case 1:
                     System.out.println("\"1. Bli medlem\\n\"");
-                    getTheSubscription();
+                    getTheSubscription();//vi kallar method d'r vi skapar medlemskap
                     break;
                 case 2:
                     System.out.println("2. Logga in\n");
@@ -58,7 +58,7 @@ public class wakeUpMain {
                     break;
                 case 3:
                     System.out.println("3. Boka plats på aktivitet\n");
-                    bookTheActivity();
+                    bookTheActivity();//vi kallar method där vi bokar aktiviteter
                     break;
                 case 4:
                     System.out.println("4. Avsluta");
@@ -74,29 +74,29 @@ public class wakeUpMain {
 
 
     private static void getTheSubscription() {
-        int monthlyPrice;
+        int monthlyPrice; //deklarerar price
         System.out.println("How long do you will stay with us");
         Scanner scanner = new Scanner(System.in);
-        int month = scanner.nextInt();
+        int month = scanner.nextInt(); //scannar antal av månader
         System.out.println(month);
-        if (month <= 2) {
+        if (month <= 2) {//om antal av dem mindre  eller lika  två pris  monthlyPrice 400
             System.out.println(400);
             monthlyPrice = 400;
-        } else if (month <= 6) {
+        } else if (month <= 6) { ////om antal av dem mindre  eller lika  sex pris  monthlyPrice 350
             System.out.println("dohera");
             monthlyPrice = 350;
-        } else if (month < 12) {
+        } else if (month < 12) {//om antal av dem mindre  tolv pris  monthlyPrice 300
             System.out.println("ewe bolshe");
             monthlyPrice = 300;
-        } else {
-            monthlyPrice = 200;
+        } else {//annars monthlyPrice 250
+            monthlyPrice = 250;
         }
         int totalPrice = monthlyPrice + 100;
         System.out.println(totalPrice);
     }
 
     private static void bookTheActivity() {
-        int choiseOfActivity=0;
+        int choiseOfActivity=0;// val av aktiviteter
         do {
             System.out.println(
                     "1. Spinning\n" +
@@ -106,48 +106,30 @@ public class wakeUpMain {
             System.out.println("Type in your choise");
             Scanner newScanner = new Scanner(System.in);
 
-             choiseOfActivity = newScanner.nextInt();
+             choiseOfActivity = newScanner.nextInt();// scannar val av aktiviteter
 
 
-        } while (choiseOfActivity < 1 || choiseOfActivity > 3);
+        } while (choiseOfActivity < 1 || choiseOfActivity > 3); // om scanade value out of range type in igen
 
-        do {
-            switch (choiseOfActivity) {
-                case 1:
-                    System.out.println("\"1. Spinningn\"");
 
-                    break;
-                case 2:
-                    System.out.println("2. Aerobic\n");
-                    System.out.println(personnummer);
-
-                    break;
-                case 3:
-                    System.out.println("3.Yoga\n");
-
-                    break;
-
-            }
-
-        } while (false);
-        String[] places = {
+        String[] places = { // skapar array som innehåller olika varianter för bookningar
                 "1a","1b","1c",
                 "2a","2b","2c",
                 "3a","3b","3c",};
-        for ( int i=0;i<places.length;i++){
-            File f = new File(choiseOfActivity+places[i]);
+        for ( int i=0;i<places.length;i++){// mest vanlig loop
+            File f = new File(choiseOfActivity+places[i]);// vi använder stream för at skapa fill med namn av bookning
 
-                try {
+                try { //hanterar fel med hjälp av try catch
 
-                    if (f.createNewFile()) {
+                    if (f.createNewFile()) {// class file innehåller inbygd method createNewFile som returnerar true om skapandet avslutad
                         System.out.println("Din plats på aktiviteten är " + places[i]);
                         break;
-                    } else {
+                    } else {//om  fil med samma namn existerar  vi får false och går till else statement där vi väntar till sista itteratrion
                         if (i == places.length-1){
-                            System.out.println("Tyvärr har vi inga lediga platser");
+                            System.out.println("Tyvärr har vi inga lediga platser");// om det finns inga lediga namn för filen då finns inga lediga platser kvar
                         }
                     }
-                } catch (IOException e) {
+                } catch (IOException e) {// Något dåligt har händ
                     System.out.println("An error occurred.");
                     e.printStackTrace();
 
@@ -155,7 +137,9 @@ public class wakeUpMain {
             }
         }
 
-    }
+    }//Enligt min mening är programmet rå och fruktansvärt skrivet för att vara ärlig hade jag inte tillräckligt med tid att avsluta det.
+    // Iögonfallande  bristen på felloggning till exempel om du anger 000 000 000 0 som personligt nummer så kommer min fruktansvärda validering inte att fungera.
+    // Under alla omständigheter med tanke på att jag arbetade ensam är jag redo att skriva om det men jag skulle vilja ha en mer detaljerad Feedback från dig eftersom jag aldrig har arbetat med OOP tidigare.
 
 
     
